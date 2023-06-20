8fe52176494a97705dee63bdf49dc779a79f8ea3
jamesrcraigie <jamesrcraigie1@gmail.com>
2023-06-20 14:12:42 +0100

    Revert "Created a base project"
    This reverts commit 51d6c0e3afc3342025c70049f2370ef38619fd8c.

--- Masters Final Project/Assets/Scripts/GameEvent.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GameEvent : MonoBehaviour
-{
-    public const string ENEMY_KILLED = "ENEMY_KILLED";
-}
