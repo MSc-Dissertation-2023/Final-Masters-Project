34f771a6d5364eee069374332b2c8b37dff2e2f3
Sheng <cyusheng93@gmail.com>
2023-06-22 14:50:26 +0100

    Created base game manager and health pickup prefab
    Multiple game managers were created along with instantiating them, logging their activities into console. Additionally, a health pickup prefab was created.

---
+++ Assets/Scripts/Game Manager/InventoryManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour, IGameManager
+{
+    public ManagerStatus status {get; private set;}
+    // Start is called before the first frame update
+    public void Startup()
+    {
+        Debug.Log("Inventory amanger starting...");
+        status = ManagerStatus.Started;
+    }
+}
