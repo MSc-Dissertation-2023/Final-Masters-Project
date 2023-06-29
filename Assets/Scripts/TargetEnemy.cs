7a50a3683479212e643f603bd20fb605b13b45cc
Sheng <cyusheng93@gmail.com>
2023-06-29 12:02:47 +0100

    Added Damage Upgrade Prefab

--- Assets/Scripts/TargetEnemy.cs
+++ Assets/Scripts/TargetEnemy.cs
@@ -11,6 +11,7 @@ public class TargetEnemy : MonoBehaviour
     private Animator animator;
     public GameObject healthPickupPrefab;
     public GameObject ammoPickupPrefab;
+    public GameObject damageUpgradePickupPrefab;
     private UIController uiController;
 
     void Start()
@@ -59,15 +60,22 @@ public class TargetEnemy : MonoBehaviour
         var enemyPos = transform.position;
         var enemyDropsPos = new Vector3(enemyPos.x, 2, enemyPos.z);
 
-        int rand = Random.Range(1, 3);
+        float rand = Random.value; // Generates a random float between 0.0 and 1.0
 
-        if (rand == 1)
+        if (rand < 0.3) // 30% chance
         {
             Instantiate(ammoPickupPrefab, enemyDropsPos, Quaternion.identity);
-        } else
+        }
+        else if (rand < 0.6) // Additional 30% chance
         {
             Instantiate(healthPickupPrefab, enemyDropsPos, Quaternion.identity);
         }
+        else if (rand < 0.75) // Additional 15% chance
+        {
+            Instantiate(damageUpgradePickupPrefab, enemyDropsPos, Quaternion.identity); // Assuming you have a damagePrefab
+        }
+        // Else there is a 25% chance to do nothing (no drop)
+
 
         yield return new WaitForSeconds(4.0f);
         Destroy(this.gameObject);
