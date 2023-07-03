39b6a39c85616ff7d4a357c36c3b269dd27f9e1e
Sheng <cyusheng93@gmail.com>
2023-07-03 10:48:22 +0100

    Fix colliding with enemy even though enemy is dead.

--- Assets/Scripts/Enemy AI/Enemy State/DyingState.cs
+++ Assets/Scripts/Enemy AI/Enemy State/DyingState.cs
@@ -20,11 +20,11 @@ public class DyingState : EnemyState
 
 		//Remove NavMesh
 		UnityEngine.AI.NavMeshAgent navMesh = enemy.GetComponent<UnityEngine.AI.NavMeshAgent>();
-		UnityEngine.Object.Destroy(navMesh);
+		navMesh.enabled = false;
 
-		enemy.GetComponent<Collider>().enabled = false;
+		enemy.GetComponent<BoxCollider>().enabled = false;
 
-		yield return new WaitForSeconds(4.0f);
+		yield return new WaitForSeconds(3.0f);
 		UnityEngine.Object.Destroy(enemy.gameObject);
 	}
 }
