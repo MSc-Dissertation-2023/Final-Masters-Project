f53f7c3e2637f3fe9e6ae3149e63f633643c76ea
jamesrcraigie <jamesrcraigie1@gmail.com>
2023-08-08 11:45:23 +0100

    Revert "Revert "Completed a few final tasks""
    This reverts commit 9e58be6ed481cc8a7360ee0237db4f260ede2220.

--- Assets/Scripts/Game Manager/DataManager.cs
+++ Assets/Scripts/Game Manager/DataManager.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class DataManager : MonoBehaviour
+public class DataManager : MonoBehaviour, IGameManager
 {
-    // Start is called before the first frame update
-    void Start()
+    public ManagerStatus status { get; private set; }
+
+    public int levelOneHealth { get; set; }
+
+    public int levelOneTime { get; set; }
+
+    
+
+    public void Startup()
     {
-        
+        Debug.Log("Data Manager manager starting...");
+
+
+        status = ManagerStatus.Started;
     }
 
-    // Update is called once per frame
-    void Update()
+    /*public void SaveStats()
     {
-        
+
+        StartCoroutine(CallAPILevelOne());
+
     }
+
+    IEnumerator CallAPILevelOne()
+    {
+
+
+        using (UnityWebRequest www = UnityWebRequest.Post($"www.mdk2023.com/StageOneStats?name={name}&score={score}", "", "application/json"))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result == UnityWebRequest.Result.Success)
+            {
+                Debug.Log("Level One API Request complete!");
+            }
+            else
+            {
+                Debug.Log(www.error);
+            }
+        }
+    }*/
 }
